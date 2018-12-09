50c3b2f8b213c0a04d9948e65d15a8a14a5c26ab
Mark Valdez <mark.valdez@rmn.com>
2018-12-08 18:35:25 -0600

    Day 1 answer

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,6 +1,9 @@
-(ns adventofcode.core)
+(ns adventofcode.core
+  (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;; Day 1 ;;;
+(with-open [rdr (io/reader "src/adventofcode/input.txt")]
+  (->> (line-seq rdr)
+       (map read-string) ;; converts to signed number
+       (reduce +) ;; sums the sequence
+       (print)))

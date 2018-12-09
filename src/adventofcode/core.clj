cb83d8a34910cbf13b0f0f840ed7e293279f0fed
Mark Valdez <mark.valdez@rmn.com>
2018-12-09 13:30:12 -0600

    Day 1 (part 2) answer

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,9 +1,25 @@
 (ns adventofcode.core
   (:require [clojure.java.io :as io]))
 
-;;; Day 1 ;;;
-(with-open [rdr (io/reader "src/adventofcode/input.txt")]
-  (->> (line-seq rdr)
-       (map read-string) ;; converts to signed number
-       (reduce +) ;; sums the sequence
-       (print)))
+(def input-file "src/adventofcode/input.txt")
+
+(def input-seq
+  (with-open [rdr (io/reader input-file)]
+    (->> (line-seq rdr)
+         (map read-string) ;; converts str to signed numbers
+         (reduce conj [])))) ;; create a vector of numbers but no longer lazy seq
+
+;;; Day 1 Part 1 ;;;
+(->> input-seq
+     (reduce +))
+
+;;; Day 1 Part 2 ;;;
+(defn search [x input acc]
+  (let [[y & more] input
+        sum (+ x y)
+        seen (conj acc sum)]
+    (if (and (not (contains? acc sum)) more)
+      (recur sum more seen)
+      (print sum))))
+
+(search 0 (cycle input-seq) #{})
